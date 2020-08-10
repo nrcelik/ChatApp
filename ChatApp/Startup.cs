702ba76cec6600707b6e2e00bfd50232fc24eb0f
nrcelik <59237286+nrcelik@users.noreply.github.com>
2020-08-10 20:32:19 +0100

    ChatApp was created

---
+++ ChatApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ChatApp.Startup))]
+namespace ChatApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
